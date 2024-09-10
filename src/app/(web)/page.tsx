2c60313ac8cacf095b37a0a43021891e02e92a5c
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-09-10 22:39:01 +0700

    Import and use FeaturedRoom component

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,17 +1,23 @@
+import FeaturedRoom from "@/components/FeaturedRoom/FeaturedRoom";
 import Gallery from "@/components/Gallery/Gallery";
 import HeroSection from "@/components/HeroSection/HeroSection";
 import NewsLetter from "@/components/NewsLetter/NewsLetter";
 import PageSearch from "@/components/PageSearch/PageSearch";
+import { getFeatureRoom } from "@/libs/apis";
 
-export default function Home() {
+const Home = async () => {
+  const featuredRoom = await getFeatureRoom();
+
   return (
     <>
       <HeroSection />
-      {/* PAGE SEARCH */}
+
       <PageSearch />
-      {/* Featured Room */}
+      <FeaturedRoom featuredRoom={featuredRoom} />
       <Gallery />
       <NewsLetter />
     </>
   );
-}
+};
+
+export default Home;
